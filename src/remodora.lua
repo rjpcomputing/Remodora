9e5134ce07ebc114aae8f8bd5fdbf044ba6d4599
rjpcomputing <rjpcomputing@gmail.com>
2012-12-19 16:15:55 -0500

    Started to get Pianobar setup/initialization working. Added a pianobar event command handler. Added icons for the interface.

--- src/remodora.lua
+++ src/remodora.lua
@@ -11,22 +11,76 @@
 --		0.0 - Initial placeholder
 -- ----------------------------------------------------------------------------
 package.path = package.path .. ";./?/init.lua"	-- Add 'init.lua' loading to the current directory
+local lfs			= require( "lfs" )
 -- Penlight
 local appSupport	= require( "pl.app" )
 local class			= require( "pl.class" )
 local lapp			= require( "pl.lapp" )
 local path			= require( "pl.path" )
 local dir			= require( "pl.dir" )
+local config		= require( "pl.config" )
 local stringx		= require( "pl.stringx" ).import()
 local pretty		= require( "pl.pretty" )
+local text			= require( "pl.text" )
+text.format_operator()
 require( "pl.strict" ); orbit = false;
 
 -- Orbiter
 local orbiter	= require( "orbiter" )
 local html		= require( "orbiter.html" )
 
+--
+local function GetStations()
+
+end
+
+local function InitializePianobar()
+	local pianobarConfigDir		= path.expanduser( "~/.config/pianobar" )
+	local pianobarConfigFile	= path.join( pianobarConfigDir, "config" )
+
+	if path.exists( pianobarConfigFile ) then
+		-- Read the config in so we can check that it matches the required configuration.
+		local pianobarConfiguration, errMsg = config.read( pianobarConfigFile )
+		if pianobarConfiguration then
+			--
+		else
+			error( ("Could not read the pianobar config file from %q/nMore info: %s"):format( pianobarConfigFile, errMsg ) )
+		end
+	else
+		dir.makepath( pianobarConfigDir )
+
+		local eventCommandPath		= path.join( pianobarConfigDir, "events.lua" )
+		local fifoPath				= path.join( pianobarConfigDir, "ctl" )
+
+		-- Write stub configuration
+		local stub =
+		[[username = user
+		password = some_password
+		volume = 0
+		event_command = $eventcommand
+		fifo = $fifo]] % { eventcommand = eventCommandPath, fifo = fifoPath }
+		-- Remove tabs
+		stub = stub:gsub( "\t", "" )
+		io.output( pianobarConfigFile ):write( stub )
+
+		-- Write the events.lua
+		local eventContents = io.input( "support/events.lua" ):read( "*a" )
+print( eventContents:format( lfs.currentdir() ) )
+		io.output( eventCommandPath ):write( eventContents:format( lfs.currentdir() ) )
+
+		-- Write fifo file
+		if not path.exists( fifoPath ) then
+			io.execute( ("mkfifo %s"):format( fifoPath ) )
+		end
+
+		return true
+	end
+
+	return false
+end
+
 -- Create the app instance
-local Remodora	= orbiter.new( html )
+local Remodora = orbiter.new( html )
 local h2, p, div, class, id		= html.tags( "h2, p, div, class, id" )
 
 -- Layout function makes the file have a template that all
@@ -35,23 +89,29 @@ function Remodora:layout( ... )
 	return html
 	{
 		title	= "Remodora v0.01",
+		favicon	= { "/images/pandora.png" },
 		styles	= { "/css/style.css" },
 		body	= { div { id = "content", ... } }
 	}
 end
 
 function Remodora:index( web )
-	-- Initialize the system
-	--
-	-- Check for FIFO file
-	if not path.exists( "ctl" ) then
-		os.execute( "mkfifo ctl" )
-	end
+	-- Make sure pianobar is running, if not start it
 
 	return self:layout
 	{
 		h2 { class = "album", "Loading" },
 		p "Web Client",
+
+	}
+end
+
+function Remodora:stations( web )
+	local stations = GetStations()
+
+	return self:layout
+	{
+		h2 ( "Stations" ),
 		html.list
 		{
 			render = html.link,
@@ -61,17 +121,23 @@ function Remodora:index( web )
 	}
 end
 
-function Remodora:sections( web, name )
-	return self:layout { h2 ( name ) }
-end
-
 -- Initialize the routes
 Remodora:dispatch_get( Remodora.index, "/", "/index.html" )
-Remodora:dispatch_get( Remodora.sections, "/section/(.+)" )
+Remodora:dispatch_get( Remodora.stations, "/stations" )
 Remodora:dispatch_static( "/css/.+" )
+Remodora:dispatch_static( "/images/.+" )
+Remodora:dispatch_static( "/js/.+" )
 
-if orbit then			-- Orbit loads the module and runs it using Xavante, etc
-    return app
-else					-- We use the Orbiter micro-server
-    Remodora:run( ... )
+-- Main entry point
+local function main( ... )
+	local isFirstRun = InitializePianobar()
+
+
+	if orbit then			-- Orbit loads the module and runs it using Xavante, etc
+		return Remodora
+	else					-- We use the Orbiter micro-server
+		Remodora:run( ... )
+	end
 end
+
+main( ... )
---
+++ src/support/events.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+-- events.lua - Pianobar event output to JSON
+local json		= require( "json" )
+local stringx	= require( "pl.stringx" )
+local tablex	= require( "pl.tablex" )
+local pretty	= require( "pl.pretty" )
+
+local out = "%s"
+
+local event = arg[1]
+local details = {}
+for line in io.stdin:lines() do
+	local detail = tablex.imap( stringx.strip, stringx.split( line, "=", 1 ) )
+	details[detail[1]] = detail[2]
+end
+
+print( pretty.write(details) )
+
+--local artist	= details.artist
+--local title		= details.title
+--local album		= details.album
+--local coverArt	= details.coverArt
+--local rating	= details.rating
+
+if "songstart" == event then
+	io.output( path.join( out, "currentSong.json" ) ):write( json.encode( details ) )
+elseif "songlove" == event then
+	-- Change the current songs rating
+	details.rating = 1
+	io.output( path.join( out, "currentSong.json" ) ):write( json.encode( details ) )
+	io.output( path.join( out, "message" ) ):write( "Loved" )
+elseif "songban" == event then
+	io.output( path.join( out, "message" ) ):write( "Banned" )
+elseif "songshelf" == event then
+	io.output( path.join( out, "message" ) ):write( "Tired" )
+end
